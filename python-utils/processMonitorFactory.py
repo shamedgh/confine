dfec500a4555fe8d00fc3b367eb5eb23822dab61
swaroop <jyothiswaroop6050@gmail.com>
2021-11-23 20:52:07 +0000

    Added functionality for Ebpf kprobe stress test

--- python-utils/processMonitorFactory.py
+++ python-utils/processMonitorFactory.py
@@ -1,9 +1,11 @@
 import sysdig
 import execsnoop
+import bpfKprobe
 
 def Factory(logger, monitorTool = "sysdig"):
     tools = {
             "sysdig": sysdig.Sysdig,
             "execsnoop": execsnoop.Execsnoop,
+            "bpfkprobe": bpfKprobe.BpfKprobe,
             }
     return tools[monitorTool](logger)
